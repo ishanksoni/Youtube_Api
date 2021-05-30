7b1f0cc15c45e6718c51176a845ce9abac500494
ishanksoni <ishanksoni8696@gmail.com>
2021-05-30 16:11:40 +0530

    rest_framework setup and models declared

--- ytApi/api/models.py
+++ ytApi/api/models.py
@@ -1,3 +1,31 @@
+from typing import Tuple
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+
+# Video table class
+class Videos(models.Model):
+
+    # ID
+    videoId = models.CharField(null = False, blank = False, max_length = 100)
+
+    # Title
+    title = models.CharField( null = True, blank = True, max_length = 100)
+
+    # Discription
+    description = models.CharField(null = True, blank = True, max_length = 1000)
+
+    # Published Time
+    publishedTime = models.DateTimeField()
+
+    # Channel ID
+    channelId = models.CharField(null = False, blank = False, max_length = 100)
+
+    # Channel Title
+    channelTitle = models.CharField(null = True, blank = True, max_length = 100)
+
+    # Current Time in db.
+    created = models.DateTimeField(null = True, blank = True, auto_now_add=True)
--- ytApi/api/views.py
+++ ytApi/api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import *
+from .serializers import *
+from rest_framework import generics
+from .pagination import *
 
-# Create your views here.
+
+class ResultsPagination(CursorPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+class VideoList(generics.ListAPIView):
+
+    queryset = Videos.objects.all().order_by('-publishedTime')
+
+    serializer_class = VideosSerializer
+    pagination_class = ResultsPagination 
